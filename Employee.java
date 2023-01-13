eb4bac6c83a0faf2f7dbecc85d32d541f87f919d
Ranjithamanjunatharao <119911650+Ranjithamanjunatharao@users.noreply.github.com>
2023-01-13 17:05:32 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+//Employee Details
+import java.lang.System;
+import java.lang.String;
+// import java.lang.*;
+class Employee{
+  public static void main(String args[]){
+      int id=101;
+String name="Karun";
+float sal=656.45F;
+      System.out.println("The given employee id is : "+id);
+      System.out.println("The given employee name is : "+name);
+      System.out.println("The given employee salary is : "+sal);
+   }
+  }
+  
