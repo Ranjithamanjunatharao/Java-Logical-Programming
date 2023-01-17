06146b47c0d927b84f7a9965071f91dd59968bc3
Ranjithamanjunatharao <119911650+Ranjithamanjunatharao@users.noreply.github.com>
2023-01-17 15:59:59 +0530

    Add files via upload

---
+++ float1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class float1
+{
+  public static void main(String args[])
+  {
+    float f;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the float value:");
+    f=sc.nextFloat();
+    System.out.println("The given string is:"+f);
+  }
+}
