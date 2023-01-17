b9089cccab5aae6b5ec8a82e91d15be5553a94c0
Ranjithamanjunatharao <119911650+Ranjithamanjunatharao@users.noreply.github.com>
2023-01-17 16:00:28 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class String1
+{
+  public static void main(String args[])
+  {
+    String str;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the string:");
+    str=sc.next();
+    System.out.println("The given string is:"+str);
+  }
+}
