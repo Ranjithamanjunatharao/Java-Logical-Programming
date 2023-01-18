f658274086689d9d3af4104f48691a0e92d72589
Ranjithamanjunatharao <119911650+Ranjithamanjunatharao@users.noreply.github.com>
2023-01-18 16:50:18 +0530

    Add files via upload

---
+++ Electricity.java
@@ -0,0 +1,31 @@
+/*Write a Java Program to enter Consumer Number , Consumer Name , Present Month Reading, Last Month Reading,
+Calculate Total units & Electricity bill and print the report.*/
+import java.util.Scanner;
+class Electricity
+  {
+     public static void main(String args[]){
+       int cno,pmr,lmr;
+       String cname;
+       double ebill;
+       double totalunit;
+       Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Consumer number");
+      cno=sc.nextInt();
+        System.out.println("Enter the Consumer name");
+      cname=sc.next();
+        System.out.println("Enter the Present month reading");
+      pmr=sc.nextInt();
+       System.out.println("Enter the Last month reading");
+      lmr=sc.nextInt();
+       totalunit=pmr-lmr;
+       ebill=totalunit *3.80;
+       System.out.println("Consumer number= "+cno);
+       System.out.println("Consumer name= "+cname);
+       System.out.println("Present month reading= "+pmr);
+        System.out.println("Last month reading= "+lmr);
+        System.out.println("Total unit= "+totalunit);
+        System.out.println("Electricity bill= "+ebill);
+     }
+  }
+       
+       
