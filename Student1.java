0f420fb4c47757d0c0ad01fb7daf59f18442ec65
Ranjithamanjunatharao <119911650+Ranjithamanjunatharao@users.noreply.github.com>
2023-03-08 00:13:22 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,16 @@
+class Student1
+  {
+    String name;
+    int rno;
+    Student1()
+    {
+      name="Ranju";
+      rno=1;
+    }
+    public static void main(String args[])
+    {
+      Student1 s1=new Student1();
+      System.out.println(s1.name);
+      System.out.println(s1.rno);
+    }
+  }
